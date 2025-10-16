095fcca6a825fee566486f6da5e74e3633438670
Mayen <111729616+MyconidM@users.noreply.github.com>
2025-10-16 16:43:22 +0200

    installed eucsessfully

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="study_reminders",
     version="0.1",
-    packages=["study_reminders"]
+    packages=find_packages(include=["study_reminders", "study_reminders.*"]),
+    include_package_data=True,
+    description="A package managing student study reminders",
+    author="Mayen E",
+    author_email="maywer2001@gmail.com",
+    install_requires=['schedule'],
+    entry_points={
+        "console_scripts": [
+            "study-reminder=study_reminders.main:main"
+        ]
+    }
 )
